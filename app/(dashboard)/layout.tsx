c11ace5b3674abdb04734293cf3664b769dcb8a1
Dipendra Singh <dipendras953@gmail.com>
2024-09-26 02:02:03 +0530

    Refactor dashboard layout and sidebar components

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,14 +1,16 @@
-import { Sidebar } from './_components/sidebar'
+import { Sidebar } from "./_components/sidebar";
 
-function DashboardLayout({children}: {children: React.ReactNode}) {
+function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className='h-full'> ise
-        <div className='hidden md:flex h-full w-56 flex-col fixed inset-y-0 z-50'>
-            <Sidebar />
-        </div>
-        {children}
+    <div className="h-full">
+      {" "}
+      ise
+      <div className="hidden md:flex h-full w-56 flex-col fixed inset-y-0 z-50">
+        <Sidebar />
+      </div>
+      {children}
     </div>
-  )
+  );
 }
 
-export default DashboardLayout  
+export default DashboardLayout;
