eda8db209022371b87d0cd93f973558ae3639a76
Dipendra Singh <dipendras953@gmail.com>
2024-10-01 01:49:44 +0530

    Refactor search functionality and add category filtering
    - Refactor the search functionality to use a debounced input for better performance.
    - Add category filtering to the search page, allowing users to filter courses by category.

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -1,11 +1,12 @@
 import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
+import SearchPage from "./search/page";
 
 
 export default function Home() {
   return (
     <>
-    <UserButton afterSwitchSessionUrl="/" />
+    <SearchPage />
     </>
   );
 }
--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -5,17 +5,25 @@ import { usePathname } from "next/navigation";
 import { Button } from "./ui/button";
 import { LogOut } from "lucide-react";
 import Link from "next/link";
+import { SearchInput } from "./search-input";
 
 export const NavbarRoutes = () => {
   const pathname = usePathname();
 
   const isTeacherPage = pathname?.startsWith("/teacher");
   const isPlayerPage = pathname?.includes("/chapter");
+  const isSearchPage = pathname === "/search"
 
   return (
+    <>
+    {isSearchPage && (
+      <div className="hidden md:block">
+        <SearchInput />
+      </div>
+    )}
     <div className="flex gap-x-2 ml-auto">
       {isTeacherPage || isPlayerPage ? (
-        <Link href="/">
+        <Link href="/search">
           <Button size="sm" variant="ghost">
             <LogOut className="h-4 w-4 mr-2" />
             Exit
@@ -30,5 +38,6 @@ export const NavbarRoutes = () => {
       )}
       <UserButton afterSwitchSessionUrl="/" />
     </div>
+    </>
   );
 };
