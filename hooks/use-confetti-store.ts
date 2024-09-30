cd783dfd2699acbd4fab0dd98636ef0912bdad49
Dipendra Singh <dipendras953@gmail.com>
2024-09-30 23:53:19 +0530

    Add use-confetti-store hook and integrate it in Actions component

--- hooks/use-confetti-store.ts
+++ hooks/use-confetti-store.ts
@@ -1,3 +1,13 @@
-import {create} from "zustand"
+import { create } from "zustand";
 
-type ConfettiS
+type ConfettiStore = {
+  isOpen: boolean;
+  onOpen: () => void;
+  onClose: () => void;
+};
+
+export const useConfettiStore = create<ConfettiStore>((set) => ({
+  isOpen: false,
+  onOpen: () => set({ isOpen: true }),
+  onClose: () => set({ isOpen: false }),
+}));
